59ff53d489964148ed6bb76382dbdad7b64129e3
zhan li <lizhan@pdx.edu>
2018-12-08 20:21:33 -0800

    set to 1 iteration

--- 02-meanshift.py
+++ 02-meanshift.py
@@ -18,7 +18,7 @@ roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
 cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)
 
 # Setup the termination criteria, either 10 iteration or move by atleast 1 pt
-term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 2, 1 )
+term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 1, 1 )
 
 while(1):
     ret ,frame = cap.read()
